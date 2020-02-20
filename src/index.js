8dff96ebf01768dc0323c09f9955c75a58794faf
NickJaNinja <ndf99551@uga.edu>
2020-02-19 19:10:13 -0500

    Adding config for our firebase db

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM, { render } from 'react-dom';
 import App from './App';
 import Init from './Init';
 import './index.css';
+import * as firebase from 'firebase';
 
-ReactDOM.render(
+var firebaseConfig = {
+    apiKey: "AIzaSyAuCrtF6WCKzY-BUrTdxdpSHEX9kq-FfZA",
+    authDomain: "ontologydb.firebaseapp.com",
+    databaseURL: "https://ontologydb.firebaseio.com",
+    projectId: "ontologydb",
+    storageBucket: "ontologydb.appspot.com",
+    messagingSenderId: "130820037060",
+    appId: "1:130820037060:web:dce897d9f50adb6fa4ac91",
+    measurementId: "G-CE6FPTZFF0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+ ReactDOM.render(
 	 <Init />,
 
 	document.getElementById('root')
