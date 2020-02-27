3e4e3fda946026aed2b064f1a3c6f426d3d08851
dbump3 <dbump3@gatech.edu>
2020-02-27 17:25:14 -0500

    name changes

--- sampleFile/nondup.py
+++
@@ -1,7 +0,0 @@
-with open('Sample-File-04.csv','r') as in_file, open('reduced04.csv','w') as out_file:
-    seen = set() # set for fast O(1) amortized lookup
-    for line in in_file:
-        if line in seen: continue # skip duplicate
-
-        seen.add(line)
-        out_file.write(line)
