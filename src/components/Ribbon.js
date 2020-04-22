5ee3d27738d89f640b3c84678ba0e8fcd1aea16b
NickJaNinja <nicholasfratto0@gmail.com>
2020-04-21 22:56:47 -0400

    undo suggestion mode button

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -23,24 +23,24 @@ class Ribbon extends Component {
 			modeText: 'Suggestion Mode'
 		}
 
-		// this.openModal = this.openModal.bind(this);
-		// this.afterOpenModal = this.afterOpenModal.bind(this);
-		// this.closeModal = this.closeModal.bind(this);
+		this.openModal = this.openModal.bind(this);
+		this.afterOpenModal = this.afterOpenModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
 		this.changeText = this.changeText.bind(this);
 	}
 	
-	// openModal() {
-	// 	this.setState({modalIsOpen: true});
-	// }
+	openModal() {
+		this.setState({modalIsOpen: true});
+	}
 
-	// afterOpenModal() {
-	// 	// references are now sync'd and can be accessed.
-	// 	this.subtitle.style.color = 'White';
-	// }
+	afterOpenModal() {
+		// references are now sync'd and can be accessed.
+		this.subtitle.style.color = 'White';
+	}
 	 
-	// closeModal() {
-	// 	this.setState({modalIsOpen: false});
-	// }
+	closeModal() {
+		this.setState({modalIsOpen: false});
+	}
 
 	changeText() {
     	if (this.state.modeText=='Suggestion Mode') this.setState({modeText: 'Edit Mode'});
@@ -51,7 +51,7 @@ class Ribbon extends Component {
 		return (
 			<div>
 				<button class="Button btn">Auto-Merge Selected Labels</button>
-		<button id="modeButton" class="Button btn" onClick={this.changeText}>{this.state.modeText}</button>
+		<button id="modeButton" class="Button btn" onClick={this.openModal}>New Label</button>
 				<Modal
 					isOpen={this.state.modalIsOpen}
 					onAfterOpen={this.afterOpenModal}
